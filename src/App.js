404b79eda01fc00cee2d190a617d6c4f11bdcc2d
d0ganoo <faucheriefrancois@gmail.com>
2019-06-25 13:47:09 +0000

    Prise en compte des css/scss et img + minification css et gzip en prod

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './assets/css/layout.css';
+import './assets/scss/layout.scss';
+import happy from './assets/img/happy.png';
+
 const App = () => {
     console.log("rze");
   return (
     <div>
       <p>React !</p>
+      <img src={happy} alt="happy-png" />
     </div>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
 const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const zopfli = require('@gfx/zopfli');
 
 module.exports = mode => {
     if (mode === "development"){
@@ -9,7 +15,7 @@ module.exports = mode => {
             entry: './src/App.js',
             watch: true,
             output: {
-                path: path.resolve('dist'),
+                path: path.resolve(__dirname, './dist'),
                 filename: 'bundle.js'
             },
             module:{
@@ -18,13 +24,39 @@ module.exports = mode => {
                         test:/\.js$/,
                         exclude: /(node_modules|bower_components)/,
                         use:['babel-loader']
+                    },
+                    {
+                        test: [/.css$|.scss$/],
+                        use:[
+                            MiniCssExtractPlugin.loader,
+                            'css-loader',
+                            'sass-loader',
+                        ]
+                    },
+                    {
+                        test: /\.(png|jpg|gif|svg)$/,
+                        use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                            name: '[name].[ext]',
+                            outputPath: 'assets/images'
+                            }
+                        }
+                        ]
                     }
                 ]
             },
             plugins: [
+                new CleanWebpackPlugin(),
                 new HtmlWebPackPlugin({
-                template: "./src/index.html",
-                filename: "./index.html"
+                    template: "./src/index.html",
+                    filename: "./index.html"
+                })
+                ,
+                new MiniCssExtractPlugin({
+                   filename: "[name].css",
+                   chunkFilename: "[id].css"
                 })
             ],
         }
@@ -36,7 +68,7 @@ module.exports = mode => {
             entry: './src/App.js',
             watch: true,
             output: {
-                path: path.resolve('dist'),
+                path: path.resolve(__dirname, './dist'),
                 filename: 'bundle.js'
             },
             module:{
@@ -45,15 +77,53 @@ module.exports = mode => {
                         test:/\.js$/,
                         exclude: /(node_modules|bower_components)/,
                         use:['babel-loader']
+                    },
+                    {
+                        test: [/.css$|.scss$/],
+                        use:[
+                        MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'sass-loader'
+                        ]
+                    },
+                    {
+                        test: /\.(png|jpg|gif|svg)$/,
+                        use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                            name: '[name].[ext]',
+                            outputPath: 'assets/images'
+                            }
+                        }
+                        ]
                     }
                 ]
             },
             plugins: [
+                new CleanWebpackPlugin(),
                 new HtmlWebPackPlugin({
-                template: "./src/index.html",
-                filename: "./index.html"
-                })
+                    inject: true,
+                    hash: true,
+                    template: "./src/index.html",
+                    filename: "./index.html",
+                }),
+                new MiniCssExtractPlugin({
+                    filename: "main.[contenthash].css",
+                    chunkFilename: "[id].css"
+                }),
+                new CompressionPlugin({
+                    compressionOptions: {
+                        numiterations: 15,
+                    },
+                    algorithm(input, compressionOptions, callback) {
+                        return zopfli.gzip(input, compressionOptions, callback);
+                    },
+                }),
             ],
+            optimization: {
+                minimizer: [new TerserPlugin(),new OptimizeCSSAssetsPlugin({})],
+            },
         }
     }
 }
